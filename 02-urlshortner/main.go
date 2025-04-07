c91d083c5bcdd645d3554dd5587f7d0cc625a698
Galdin Raphael <gldraphael@users.noreply.github.com>
2025-04-07 23:56:00 +0100

    Support json config files too

--- 02-urlshortner/main.go
+++ 02-urlshortner/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"gopkg.in/yaml.v3"
 )
@@ -17,17 +20,28 @@ type RedirectionConfig struct {
 
 func main() {
 
-	fileText, err := os.ReadFile("./config.yaml")
+	configFilePath := flag.String("file", "./config.yaml", "specify a path to the config file")
+	flag.Parse()
+
+	var config RedirectionConfig
+	var err error
+	fileFormat := filepath.Ext(*configFilePath)
+	switch fileFormat {
+	case ".yaml":
+		fallthrough
+	case ".yml":
+		config, err = getYaml(*configFilePath)
+	case ".json":
+		config, err = getJson(*configFilePath)
+	default:
+		log.Fatal("Invalid file format ", fileFormat, ". Only yaml and json files are supported.")
+		return
+	}
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-
-	var config RedirectionConfig
-	if err := yaml.Unmarshal([]byte(fileText), &config); err != nil {
-        log.Fatal(err)
-		return
-	}
+	
 
 	for _, rule := range config.Redirects {
 		http.HandleFunc(rule.Slug, func(w http.ResponseWriter, r *http.Request) {
@@ -36,4 +50,43 @@ func main() {
 	}
 
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func getYaml(filePath string) (RedirectionConfig, error) {
+
+	fileText, err := getFileText(filePath)
+	if err != nil {
+		return RedirectionConfig{}, err
+	}
+
+	var config RedirectionConfig
+	if err := yaml.Unmarshal([]byte(fileText), &config); err != nil {
+        log.Fatal(err)
+		return RedirectionConfig{}, err
+	}
+	return config, nil
+}
+
+func getJson(filePath string) (RedirectionConfig, error) {
+
+	fileText, err := getFileText(filePath)
+	if err != nil {
+		return RedirectionConfig{}, err
+	}
+
+	var config RedirectionConfig
+	if err := json.Unmarshal([]byte(fileText), &config); err != nil {
+        log.Fatal(err)
+		return RedirectionConfig{}, err
+	}
+	return config, nil
+}
+
+func getFileText(filePath string) (string, error) {
+	fileText, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+	return string(fileText), nil
 }
