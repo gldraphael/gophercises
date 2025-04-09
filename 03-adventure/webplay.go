c56e8e52d410624e5fb201d45769d4827c32f765
Galdin Raphael <gldraphael@users.noreply.github.com>
2025-04-09 13:09:30 +0100

    Complete the web implementation with UI

--- 03-adventure/webplay.go
+++ 03-adventure/webplay.go
@@ -1,6 +1,26 @@
 package main
 
-func WebModeGamePlay(story AdventureStory) {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
 
+func WebModeGamePlay(story AdventureStory) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, fmt.Sprintf("/%s", story.FirstArc), http.StatusTemporaryRedirect)
+	})
+
+	tmpl := template.Must(template.ParseFiles("./template.html"))
+	for key, arc := range story.StoryArcs {
+		http.HandleFunc(fmt.Sprintf("/%s", key), func(w http.ResponseWriter, r *http.Request) {
+      tmpl.Execute(w, arc)
+		})
+	}
+
+
+	log.Println("Listening at localhost:8080")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
